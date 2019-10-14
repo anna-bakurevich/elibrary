7c8030230c9c27bd46629838af56946648e32708
Anna Bakurevich <annabaku0505@gmail.com>
2019-10-14 16:22:44 +0300

    edited encoding filter

--- web/src/main/java/com/jd2/elibrary/web/filter/EncodingFilter.java
+++ web/src/main/java/com/jd2/elibrary/web/filter/EncodingFilter.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
     private static final String UTF_8 = "UTF-8";
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
@@ -17,11 +18,16 @@ public class EncodingFilter implements Filter {
         request.setCharacterEncoding(UTF_8);
         response.setCharacterEncoding(UTF_8);
 
+        //достаем локаль из запроса
         String locale = request.getParameter("locale");
-
-        if (locale==null){
-                request.getSession().setAttribute("locale", "ru_RU");
-        }else {
+        //если локаль не определена, пытаемся достать ее из сессии
+        if (locale == null) {
+            locale = (String) request.getSession().getAttribute("locale");
+        }
+        //если и в запросе и в сессии локаль не определена, устанавливаем русскую, иначе - текущую
+        if (locale == null) {
+            request.getSession().setAttribute("locale", "ru_RU");
+        } else {
             request.getSession().setAttribute("locale", locale);
         }
         filterChain.doFilter(request, response);
